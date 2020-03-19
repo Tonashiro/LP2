8728b2eeb1d59670bfb871d36accac691135ffaa
Vinicius Tonashiro <61607403+Tonashiro@users.noreply.github.com>
2020-03-18 21:31:29 -0300

    Add files via upload

---
+++ Atividade2/PesoIdeal/ProjetoPesoIdeal/ProjetoPesoIdeal/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProjetoPesoIdeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn01 = new System.Windows.Forms.Button();
+            this.lbl01 = new System.Windows.Forms.Label();
+            this.lbl02 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioFem = new System.Windows.Forms.RadioButton();
+            this.radioMasc = new System.Windows.Forms.RadioButton();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn01
+            // 
+            this.btn01.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(99, 205);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(181, 66);
+            this.btn01.TabIndex = 0;
+            this.btn01.Text = "calcular";
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.btn01_Click);
+            // 
+            // lbl01
+            // 
+            this.lbl01.AutoSize = true;
+            this.lbl01.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl01.Location = new System.Drawing.Point(71, 76);
+            this.lbl01.Name = "lbl01";
+            this.lbl01.Size = new System.Drawing.Size(42, 20);
+            this.lbl01.TabIndex = 1;
+            this.lbl01.Text = "Altura";
+            // 
+            // lbl02
+            // 
+            this.lbl02.AutoSize = true;
+            this.lbl02.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl02.Location = new System.Drawing.Point(71, 148);
+            this.lbl02.Name = "lbl02";
+            this.lbl02.Size = new System.Drawing.Size(72, 20);
+            this.lbl02.TabIndex = 2;
+            this.lbl02.Text = "Peso atual";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(119, 77);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(78, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(152, 148);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioFem);
+            this.groupBox1.Controls.Add(this.radioMasc);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(324, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 90);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "       Gênero";
+            // 
+            // radioFem
+            // 
+            this.radioFem.AutoSize = true;
+            this.radioFem.Location = new System.Drawing.Point(7, 59);
+            this.radioFem.Name = "radioFem";
+            this.radioFem.Size = new System.Drawing.Size(82, 24);
+            this.radioFem.TabIndex = 1;
+            this.radioFem.Text = "Feminino";
+            this.radioFem.UseVisualStyleBackColor = true;
+            this.radioFem.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioMasc
+            // 
+            this.radioMasc.AutoSize = true;
+            this.radioMasc.Checked = true;
+            this.radioMasc.Location = new System.Drawing.Point(7, 35);
+            this.radioMasc.Name = "radioMasc";
+            this.radioMasc.Size = new System.Drawing.Size(87, 24);
+            this.radioMasc.TabIndex = 0;
+            this.radioMasc.TabStop = true;
+            this.radioMasc.Text = "Masculino";
+            this.radioMasc.UseVisualStyleBackColor = true;
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Location = new System.Drawing.Point(331, 205);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(0, 13);
+            this.labelOut.TabIndex = 6;
+            this.labelOut.Click += new System.EventHandler(this.labelAVISO_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 338);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lbl02);
+            this.Controls.Add(this.lbl01);
+            this.Controls.Add(this.btn01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Label lbl01;
+        private System.Windows.Forms.Label lbl02;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioFem;
+        private System.Windows.Forms.RadioButton radioMasc;
+        private System.Windows.Forms.Label labelOut;
+    }
+}
+
---
+++ Atividade2/PesoIdeal/ProjetoPesoIdeal/ProjetoPesoIdeal/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPesoIdeal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn01_Click(object sender, EventArgs e)
+        {
+            double altura, peso;
+            double imc = 0;
+            
+                if (double.TryParse(txtPeso.Text, out peso) && double.TryParse(txtAltura.Text, out altura))
+                {
+                    if (radioMasc.Checked)
+                    {
+                        imc = Math.Round((72.7 * altura) - 58.0,2);
+                    }
+                    else if (radioFem.Checked)
+                    {
+                        imc = Math.Round((62.1 * altura) - 44.7,2);
+                    }
+                    else
+                        MessageBox.Show("Selecione Sexo");  
+                }
+                else
+                {
+                    MessageBox.Show("Forneça valores");
+                }
+
+                if (imc < peso)
+                {
+                    labelOut.Text = "Você está acima do peso.";
+                }
+                else if (imc > peso)
+                {
+                    labelOut.Text = "Você está abaixo do peso.";
+                }
+                else if (imc == peso) 
+                { 
+                    labelOut.Text = "Você está no peso ideal"; 
+                }                     
+        }
+
+        private void labelAVISO_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
