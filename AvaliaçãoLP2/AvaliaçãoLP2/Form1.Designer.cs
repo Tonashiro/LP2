e26e0e690847893a88bd7bdfc4c0e08fb66e0c1a
Vinicius Tonashiro <61607403+Tonashiro@users.noreply.github.com>
2020-06-19 20:32:55 -0300

    Add files via upload

---
+++ AvaliaçãoLP2/AvaliaçãoLP2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AvaliaçãoLP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(206, 162);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(147, 95);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(487, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 394);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AvaliaçãoLP2/AvaliaçãoLP2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace AvaliaçãoLP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] vendas = new double[10, 4];
+            string valor = "";
+            double[] valSemana = new double[4];
+            double[] valMes = new double[10];
+            double valTotal = 0;
+
+            for (var i = 0; i < 10; i++)
+
+                for (var j = 0; j < 4; j++)
+                {
+                    valor = Interaction.InputBox("Digite o valor da semana:" + (j + 1), "Entrada de dados");
+
+                    if (valor == "")
+                        break;
+
+                    if (Double.TryParse(valor, out vendas[i, j]))
+                    {
+                        valSemana[j] = Convert.ToDouble(valor);
+                        valMes[i] += valSemana[j];
+                        valTotal += valSemana[j];
+
+                        listBox1.Items.Add("Total vendido no mês: " + (i + 1) + " Semana: " + (j + 1) + " R$ " + valSemana[j] + "\n");
+
+                        if (j == 3)
+                        {
+                            listBox1.Items.Add(">> Total vendido no mês: " + "R$ " + valMes[i] + "\n");
+                        }
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Número inválido!");
+                        j--;
+                    }
+                }
+            listBox1.Items.Add("Total de vendas geral: " + "R$ "+valTotal);
+        }
+    }
+ }
+
